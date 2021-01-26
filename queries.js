2ff760d59364af0eaaad4b35adc405251621b242
bhavyagupta1 <48151042+bhavyagupta1@users.noreply.github.com>
2021-01-26 16:28:54 +0530

    Update queries.js

--- queries.js
+++ queries.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   port: 5432,
 })
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM userss ORDER BY id ASC', (error, results) => {
+  pool.query('SELECT * FROM userss', (error, results) => {
     if (error) {
       throw error
     }
@@ -16,9 +16,9 @@ const getUsers = (request, response) => {
 }
 
 const getUserById = (request, response) => {
-  const id = parseInt(request.params.id)
+  const Region = parseInt(request.params.Region)
 
-  pool.query('SELECT * FROM userss WHERE id = $1', [id], (error, results) => {
+  pool.query('SELECT * FROM userss WHERE Region = $1', [Region], (error, results) => {
     if (error) {
       throw error
     }
@@ -26,48 +26,8 @@ const getUserById = (request, response) => {
   })
 }
 
-const createUser = (request, response) => {
-  const { name, email } = request.body
-
-  pool.query('INSERT INTO userss (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`User added with ID: ${result.insertId}`)
-  })
-}
-
-const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
-  const { name, email } = request.body
-
-  pool.query(
-    'UPDATE userss SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User modified with ID: ${id}`)
-    }
-  )
-}
-
-const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('DELETE FROM userss WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).send(`User deleted with ID: ${id}`)
-  })
-}
 
 module.exports = {
   getUsers,
   getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
 }
